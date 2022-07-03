3109c6287e79de357bc72989f49c253134861798
Devansh <devansh8954@gmail.com>
2022-07-03 13:26:43 +0530

    updated condtion

--- calculator.cpp
+++ calculator.cpp
@@ -17,6 +17,8 @@ class calculator
         myObj.num1;
         myObj.num2;
         myObj.op;
+        myObj.ans;
+
         cout <<"Enter number 1" << endl;
         cin >> myObj.num1;
         cout <<"Enter number 2" << endl;
@@ -24,6 +26,41 @@ class calculator
         cout <<"Enter an operator (+, -, *, /, %):" << endl;
         cin >> myObj.op;
 
+        switch(myObj.op) {
+         case '+': myObj.ans = myObj.num1 + myObj.num2;
+            break;
+         case '-':
+         if(myObj.num1>myObj.num2)
+         {
+            myObj.ans = myObj.num1 - myObj.num2;
+         } 
+         else{
+            myObj.ans = myObj.num2 - myObj.num1;
+         }
+            break;
+         case '*': myObj.ans = myObj.num1 * myObj.num2;
+            break;
+         case '/':
+         if(myObj.num1>myObj.num2)
+         {
+            myObj.ans = myObj.num1 / myObj.num2;
+         } 
+         else{
+            myObj.ans =myObj.num2 / myObj.num1;
+         }
+            break;
 
-        return 0;
-    }
+      default: cout <<"Error! Enter correct operator";
+         return;
+
+          cout <<"The result of the input is as given below :";
+       if(myObj.num1>myObj.num2){
+         cout << myObj.num1 << " " << myObj.op << " " << myObj.num2 <<" = " << myObj.ans;
+        }
+         else{
+            cout << myObj.num2 << " " << myObj.op << " " << myObj.num1 << " = " << myObj.ans;
+        }
+   }
+
+    return 0;
+}
