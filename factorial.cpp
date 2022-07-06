3212b06d96fe5729df4428169a94d9e84ec02063
Rishabh Jain <91656132+Rishabh2004@users.noreply.github.com>
2022-07-06 21:58:01 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-int fac=1,num;
+int fac=1,num1;
 
 void fact()
 {
     cout <<"Enter a number" << endl;
-    cin >> num;
-     if((num==0)||(num<0))
+    cin >> num1;
+     if((num1==0)||(num1<0))
      {
         cout << "Factorial is 1";    
      }
      else{
-     for(int i=1;i<=num;i++)
+     for(int i=1;i<=num1;i++)
      {    
          fac=fac*i;         
      }
-     cout << "Factorial of " << num <<" is: " << fac << endl;
+     cout << "Factorial of " << num1 <<" is: " << fac << endl;
     }
 }
 
